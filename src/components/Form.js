e2430b4cdb5b46eff178b4ce4afc1c525e76eb1f
misskip <anita.desanmiguel@gmail.com>
2022-01-04 14:46:46 +1100

    Completed Assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,80 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+function Form() {
+  const initialValues = {username: "", email: "" , password: "" };
+  const [formValues, setFormValues]= useState(initialValues);
+  const [formErrors, setFormErrors]= useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const {name, value } = e.target;
+    setFormValues({...formValues, [name]: value });
+    console.log(formValues);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  const validate = (values) => {
+    const errors = {} ;
+    //const regex = validate email 
+    if (!values.username) {
+      errors.username = "Username required";
+    }
+    if (!values.email) {
+      errors.username = "Email required";
+    }
+    if (!values.password) {
+      errors.username = "Password required";
+    }
+  return errors;  
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
+    <div className="container">
+      <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      <form onSubmit={handleSubmit}>
+        <h1> Login Form</h1>
+        <div className= "ui divider"></div>
+        <div className= "ui form">
+          <div className="field">
+            <label> Username </label>
+            <input 
+            type="text" 
+            name="username" 
+            placeholder="Username" 
+            value={formValues.username}
+            onChange={handleChange}/>
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input 
+            type="email" 
+            name="email" 
+            placeholder="Email" 
+            value={formValues.email}
+            onChange={handleChange}/>
+            </div>
+          <div className="field">
+            <label>Password</label>
+            <input 
+            type="password" 
+            name="password" 
+            placeholder="Password" 
+            value={formValues.password}
+            onChange={handleChange}/>
+
+      
+          </div>
+          <button className="fluid ui button blue">Submit</button>
+        </div>
+      </form>
+      </div>
+  )
 }
 
 export default Form;
